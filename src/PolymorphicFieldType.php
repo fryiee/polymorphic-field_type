e9defddf2137cf8cb422e3925ee1637119bdca74
Ryan Thompson <service@aiwebsystems.com>
2014-08-31 04:21:39 -0500

    Initial commit

---
+++ src/PolymorphicFieldType.php
@@ -0,0 +1,32 @@
+<?php namespace Streams\Addon\FieldType\Field;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class PolymorphicFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     * This column uses a couple custom columns.
+     *
+     * @var bool
+     */
+    protected $columnType = false;
+
+    /**
+     * Add the columns when assigned.
+     *
+     * @param $assignment
+     */
+    public function onAssigned($assignment)
+    {
+        $table = $assignment->getStream()->tableName();
+
+        \Schema::table(
+            $table,
+            function ($table) use ($assignment) {
+                $table->string($assignment->field->slug . '_string')->nullable();
+                $table->integer($assignment->field->slug . '_id')->nullable();
+            }
+        );
+    }
+}
