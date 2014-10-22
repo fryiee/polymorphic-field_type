008c0c938c64204236885c798c895c2a55a4ba73
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:40:06 -0500

    Clean up before final

--- src/PolymorphicFieldType.php
+++ src/PolymorphicFieldType.php
@@ -4,21 +4,6 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class PolymorphicFieldType extends FieldTypeAddon
 {
-    protected $slug = 'polymorphic';
-
-    /**
-     * The database column type this field type uses.
-     * This column uses a couple custom columns.
-     *
-     * @var bool
-     */
-    protected $columnType = false;
-
-    /**
-     * Add the columns when assigned.
-     *
-     * @param $assignment
-     */
     public function onAssigned($assignment)
     {
         $table = $assignment->stream->entryTable();
