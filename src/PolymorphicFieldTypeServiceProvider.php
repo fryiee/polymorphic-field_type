780f003cb59c2168e0326fb24b1a150506142805
Ryan Thompson <ryan@anomaly.is>
2015-03-24 12:50:27 -0500

    Update PolymorphicFieldTypeServiceProvider.php

--- src/PolymorphicFieldTypeServiceProvider.php
+++ src/PolymorphicFieldTypeServiceProvider.php
@@ -5,11 +5,13 @@ use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
 class PolymorphicFieldTypeServiceProvider extends AddonServiceProvider
 {
 
+    /*
     protected $routes = [
         'anomaly/field_type/polymorphic/search' => [
             'uses' => 'Anomaly\PolymorphicFieldType\PolymorphicFieldTypeController@search',
             'as'   => 'anomaly.field_type.polymorphic.search'
         ],
     ];
+    */
 
 }
